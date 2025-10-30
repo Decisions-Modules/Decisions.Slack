14312a7fc7c4e0ad5e5e5be9d76b28d18668af44
jeff-decisions <jeff.bordeaux@decisions.com>
2025-10-30 14:40:21 -0400

    Updating for second folder

--- Decisions.Slack/FolderBehaviors/SlackBotsFolderBehavior.cs
+++ Decisions.Slack/FolderBehaviors/SlackBotsFolderBehavior.cs
@@ -1,28 +1,40 @@
 using DecisionsFramework.ServiceLayer.Actions;
 using DecisionsFramework.ServiceLayer.Actions.Common;
 using DecisionsFramework.ServiceLayer.Services.Folder;
+using DecisionsFramework.ServiceLayer.Services.Projects;
 
 namespace Decisions.Slack.FolderBehaviors;
 
-public class SlackBotsFolderBehavior : DefaultFolderBehavior
+public class SlackBotsFolderBehavior : DefaultFolderBehavior, ILookAndFeelProviderFolderBehavior
 {
     internal const string NAME = "Bots";
 
     public override string FolderBehaviorName => NAME;
     public override bool IsExportable(Folder f) => true;
     public override bool ExportChildrenOnly(Folder f) => false;
-    
+
     public override BaseActionType[] GetFolderActions(Folder folder, BaseActionType[] proposedActions, EntityActionType[] types)
     {
         List<BaseActionType> actions = new List<BaseActionType>();
         actions.AddRange(base.GetFolderActions(folder, proposedActions, types));
-        
+
         actions.AddRange([
-            new AddEntityAction(typeof(SlackBot), "Add Bot", "Adds a new Slack Bot entity", null, "Add Slack Bot") 
+            new AddEntityAction(typeof(SlackBot), "Add Bot", "Adds a new Slack Bot entity", null, "Add Slack Bot")
                 { Order = 5, DisplayType = ActionDisplayType.Primary }
 
         ]);
 
         return actions.ToArray();
+    }
+    
+    private const string BOT_FOLDER_LOGO = "../Content/CustomModuleImages/Decisions.Slack/slackbot.png";
+    public LookAndFeel GetLookAndFeel(string folderId)
+    {
+        LookAndFeel lookandfeel = new LookAndFeel(folderId, FolderStructureHelper.Project_Default_SubFolder_Color, null, 
+            new DecisionsFramework.ServiceLayer.Services.Image.ImageInfo()
+            {
+                ImageType = DecisionsFramework.ServiceLayer.Services.Image.ImageInfoType.Url, ImageUrl = BOT_FOLDER_LOGO
+            });
+        return lookandfeel;
     }
 }
